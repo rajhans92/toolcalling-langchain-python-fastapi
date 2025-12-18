0e072294e59e07db81311470882cef529d36fa41
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2025-12-18 11:57:28 +0530

    model setup

--- backend/app/agents/toolCalling.py
+++ backend/app/agents/toolCalling.py
@@ -1,6 +1,25 @@
+from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
+from langchain.messgaeables import HumanMessage
+from app.agents.tools import get_weather, get_aqi, get_cost_of_living
 
 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-def toolCallingAi():
-    return "toolCallingAi"
+def toolCallingAi(question: str) -> str:
+    try:
+        tools = [get_weather, get_aqi, get_cost_of_living]
+
+        agent = create_agent(
+            model=llm,
+            tools=tools,
+            system_prompt="You are a city information assistant."
+        )
+
+        result = agent.invoke({"messages": [HumanMessage(content=question)]})
+
+        print(result["messages"][-1].content)
+
+        return result["messages"][-1].content
+    except Exception as e:
+        raise Exception(f"Error in toolCallingAi: {str(e)}") from e
--- backend/app/routers/aiRoute.py
+++ backend/app/routers/aiRoute.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/ai", tags=["ai-tool-calling"])
 @router.get("/tool-calling")
 async def tool_calling(question: str = Field(...), user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
-        reply = toolCallingAi()
+        reply = toolCallingAi(question)
         return {"message": reply}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
